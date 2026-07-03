79e421c6b3249c2768804b6c16b7e7e7dd45a119
sasakiryo <ryo.s34@icloud.com>
2026-07-04 08:39:09 +0900

    add metadata

--- src/app/(mainPages)/tier-list/page.tsx
+++ src/app/(mainPages)/tier-list/page.tsx
@@ -1,12 +1,18 @@
 import TierList from "../../../components/tier-list/TierList"
 
+export const metadata = {
+  title: "OPBR Tier List | Best Characters in One Piece Bounty Rush",
+  description:
+    "Check the latest OPBR tier list for One Piece Bounty Rush. Find the best characters ranked by current meta strength, league battle performance, roles, and overall usefulness. Stay updated with the top picks for your team.",
+}
+
 export default function TierListPage() {
   return (
     <div>
 
       {/* タイトル */}
       <div>
-      <p>OPBR Character Tier List</p>
+      <h1>OPBR Character Tier List</h1>
       <p>7/3 10:00 updated</p>
       </div>
 
