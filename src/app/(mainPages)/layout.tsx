915d22555cd1cabe9451075abc0df0dcc05a2bdd
sasakiryo <ryo.s34@icloud.com>
2026-07-10 15:43:27 +0900

    fix app/layout.tsx (mainpages)/layout.tsx metadata

--- src/app/(mainPages)/layout.tsx
+++ src/app/(mainPages)/layout.tsx
@@ -1,26 +1,4 @@
 import CommonHeader from "@/components/header/CommonHeader";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  verification:{ "google": "3E7MzcTwoRzsGYr3UPygrqEsML__uod9wisR7CDMdes" 
-  },
-
-  title: {
-    default: "OPBR Tier List & Guide | One Piece Bounty Rush",
-    template: "%s | OPBR Tier List & Guide",
-  },
-
-  description: 
-  "Tier lists, character rankings, and guides for One Piece Bounty Rush.",
-
-  applicationName: "OPBR Tier List & Guide",
-
-  icons: {
-    icon: "/favicon-2.png",
-    shortcut: "/favicon-2.png",
-    apple: "/favicon-2.png",
-  },
-};
 
 export default function EnLayout({
   children,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,30 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
+export const metadata: Metadata = {
+  verification:{ "google": "3E7MzcTwoRzsGYr3UPygrqEsML__uod9wisR7CDMdes" 
+  },
+
+  title: {
+    default: "OPBR Tier List & Guide | One Piece Bounty Rush",
+    template: "%s | OPBR Tier List & Guide",
+  },
+
+  description: 
+  "Tier lists, character rankings, and guides for One Piece Bounty Rush.",
+
+  applicationName: "OPBR Tier List & Guide",
+
+  icons: {
+    icon: "/favicon-2.png",
+    shortcut: "/favicon-2.png",
+    apple: "/favicon-2.png",
+  },
+};
 
 export default function RootLayout({
   children,
