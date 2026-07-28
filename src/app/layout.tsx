a8d708a5aee96ebd7f0309ae4dfd204c78595181
sasakiryo <ryo.s34@icloud.com>
2026-07-29 07:31:34 +0900

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -54,6 +55,7 @@ export default function RootLayout({
           }}
         />
         {children}
+        <Analytics/>
       </body>
     </html>
   );
