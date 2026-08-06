d2bb15ad02e7e79478b8570493223513833a8112
ryo650 <ryo.s34@icloud.com>
2026-08-06 10:25:22 +0900

    migrate character guides to current data model

--- src/data/character-guides/index.ts
+++ src/data/character-guides/index.ts
@@ -1,8 +1,10 @@
 import { sSnakeGuide } from "./seraphim-s-snake";
+import { stMarcusMarsGuide } from "./the-five-elders-st-marcus-mars";
 import type { CharacterGuide } from "./type";
 
 export const characterGuides: Record<string, CharacterGuide> = {
   [sSnakeGuide.characterId]: sSnakeGuide,
+  [stMarcusMarsGuide.characterId]: stMarcusMarsGuide,
 };
 
 export function getCharacterGuide(characterId: string) {
