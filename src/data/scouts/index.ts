98349f58cd441e1486f978e7114a765b380380d1
sasakiryo <ryo.s34@icloud.com>
2026-08-18 10:58:03 +0900

    fix 7-5exSabo

--- src/data/scouts/index.ts
+++ src/data/scouts/index.ts
@@ -6,7 +6,7 @@ import { bfDoressrosa } from "./doressrosabf";
 import { eggheadStMarz } from "./st-marz";
 import { eggheadBfKaku } from "./7-5bfKaku";
 import { exZephyr } from "./7-5exZephyr";
-import { exFlameEmperorSabo } from "./7-5ExSabo";
+import { exFlameEmperorSabo } from "./7-5exSabo";
 import { exHungerPangsCharlotteLinlin } from "./7-5exlinlin";
 import { exBlackbeardPiratesKuzan } from "./7-5exKuzan";
 
