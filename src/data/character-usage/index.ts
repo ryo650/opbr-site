1728b2f596254369d69224e615b65c859083c95c
sasakiryo <ryo.s34@icloud.com>
2026-08-19 09:08:12 +0900

    add data character usage 0819

--- src/data/character-usage/index.ts
+++ src/data/character-usage/index.ts
@@ -1,7 +1,9 @@
 import { characterUsage20260802 } from "./snapshots/2026-08-02";
+import { characterUsage20260819 } from "./snapshots/2026-08-19";
 
 export const characterUsageSnapshots = [
   characterUsage20260802,
+  characterUsage20260819,
 ].sort(
   (a, b) =>
     new Date(a.date).getTime() -
