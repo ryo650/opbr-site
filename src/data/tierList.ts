fc2adc7b2459e31f8418a0e7f51fe4eb1a5826ea
sasakiryo <ryo.s34@icloud.com>
2026-08-26 15:52:53 +0900

    add data/tierlist0826

--- src/data/tierList.ts
+++ src/data/tierList.ts
@@ -12,26 +12,26 @@ export const tierList = [
     {
         tier: "S",
         colorClass: "S",
-        characterIds: ["winner-island-trafalgar-law","flame-emperor-sabo","the-five-elders-st-ethanbaron-v-nusjuro"]
+        characterIds: ["the-five-elders-st-ethanbaron-v-nusjuro","the-wings-zoro-sanji","the-five-elders-st-marcus-mars"]
     },
     {
         tier: "A",
         colorClass: "A",
-        characterIds: ["blackbeard-pirates-kuzan","the-fouremperors-marshall-d-teach","great-pirate-gol-d-roger","red-rock-monkey-d-luffy","father-and-daughter-kuma-bonny"]
+        characterIds: ["winner-island-trafalgar-law","flame-emperor-sabo","blackbeard-pirates-kuzan","the-fouremperors-marshall-d-teach","animal-kingdom-pirates-lead-performer-king"]
     },
     {
         tier: "B",
         colorClass: "B",
-        characterIds: ["the-five-elders-st-marcus-mars","the-wings-zoro-sanji","ama-no-murakumo-sword-kizaru","legendary-gladiator-kyros","animal-kingdom-pirates-lead-performer-king","germa-66-vinsmoke-niji","egghead-tony-tony-chopper","unexpected-collaboration-rob-lucci"]
+        characterIds: ["red-rock-monkey-d-luffy","great-pirate-gol-d-roger","father-and-daughter-kuma-bonny","ama-no-murakumo-sword-kizaru","battle-of-monsters-on-onigashima-kaido","unexpected-collaboration-rob-lucci"]
     },
     {
         tier: "C",
         colorClass: "C",
-        characterIds: ["great-pirate-edward-newgate","seraphim-s-snake","egghead-roronoa-zoro","egghead-brook","kaido-s-son-yamato","battle-of-monsters-on-onigashima-kaido"]
+        characterIds: ["legendary-gladiator-kyros","great-pirate-edward-newgate","seraphim-s-snake",,"germa-66-vinsmoke-niji","egghead-tony-tony-chopper","egghead-roronoa-zoro","kaido-s-son-yamato"]
     },
     {
         tier: "D",
         colorClass: "D",
-        characterIds: ["heir-to-the-shogun-of-wano-kozuki-oden","the-five-elders-st-jaygarcia-saturn","film-z-zephyr","awakened-form-rob-lucci","asl-ace-sabo-luffy","egghead-sanji"]
+        characterIds: ["heir-to-the-shogun-of-wano-kozuki-oden","egghead-brook","the-five-elders-st-jaygarcia-saturn","film-z-zephyr","awakened-form-rob-lucci","asl-ace-sabo-luffy","egghead-sanji"]
     }
 ]
