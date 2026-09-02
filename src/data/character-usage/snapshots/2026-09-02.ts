7b3a021bafdd89b4a8af3a018c7220cdc3506845
sasakiryo <ryo.s34@icloud.com>
2026-09-02 10:11:34 +0900

    add data/character-usage0902

---
+++ src/data/character-usage/snapshots/2026-09-02.ts
@@ -0,0 +1,32 @@
+import type { CharacterUsageSnapshot } from "../type";
+
+export const characterUsage20260902= {
+    date: "2026-09-02",
+    targetPlayers: 100,
+
+    usage: {
+        "the-four-emperors-monkey-d-luffy": 31,
+        "the-wings-zoro-sanji": 29,
+        "the-five-elders-st-marcus-mars": 15,
+        "seraphim-s-snake": 4,
+        "the-five-elders-st-ethanbaron-v-nusjuro": 21,
+        "ama-no-murakumo-sword-kizaru": 2,
+        "flame-emperor-sabo": 3,
+        "animal-kingdom-pirates-lead-performer-king": 13,
+        "father-and-daughter-kuma-bonny": 6,
+        "blackbeard-pirates-kuzan": 4,
+        "unexpected-collaboration-rob-lucci": 5,
+        "winner-island-trafalgar-law": 7,
+        "great-pirate-edward-newgate": 4,
+        "great-pirate-gol-d-roger": 5,
+        "egghead-tony-tony-chopper": 3,
+        "the-fouremperors-marshall-d-teach": 7,
+        "future-where-i-m-the-most-free-jewelry-bonny": 36,
+        "red-rock-monkey-d-luffy": 1,
+        "divine-departure-shanks": 1,
+        "seraphim-s-shark": 1,
+        "sick-sick-fruit-doc-q": 1,
+        "legendary-hero-monkey-d-garp": 1,
+        "raid-on-onigashima-roronoa-zoro": 1,
+    },
+} satisfies CharacterUsageSnapshot;
