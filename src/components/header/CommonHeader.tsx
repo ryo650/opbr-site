3dbb350936f217056285e5d077e0e5c2ddbb210d
sasakiryo <ryo.s34@icloud.com>
2026-09-08 10:00:11 +0900

    fix: improve site accessibility and navigation

--- src/components/header/CommonHeader.tsx
+++ src/components/header/CommonHeader.tsx
@@ -86,6 +86,7 @@ export default function CommonHeader() {
     <header
       ref={headerRef}
       className={`${styles.header} ${isHidden ? styles.headerHidden : ""}`}
+      onFocusCapture={() => setIsHidden(false)}
     >
       {/* 現在の .surface 以下はそのまま */}
       <div className={styles.surface}>
@@ -105,7 +106,7 @@ export default function CommonHeader() {
                 <span>Menu</span>
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className={styles.menuContent} onCloseAutoFocus={(event) => event.preventDefault()}>
+            <DropdownMenuContent className={styles.menuContent}>
               {navigationItems.map((item) => (
                 <DropdownMenuItem key={item.href} asChild className={styles.menuItem}>
                   <Link href={item.href}>{item.label}</Link>
@@ -123,7 +124,7 @@ export default function CommonHeader() {
             width={72}
             height={72}
             className={styles.logo}
-            priority
+            preload
           />
         </Link>
 
