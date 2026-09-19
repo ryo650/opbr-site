063d9879c2a9113974af3afcbe2dafc1c53ad872
sasakiryo <ryo.s34@icloud.com>
2026-09-19 10:33:24 +0900

    add data/characterUsage 0919

---
+++ src/data/character-usage/snapshots/2026-09-19.ts
@@ -0,0 +1,37 @@
+import type { CharacterUsageSnapshot } from "../type";
+
+export const characterUsage20260919= {
+    date: "2026-09-19",
+    targetPlayers: 100,
+
+    usage: {
+        "the-four-emperors-monkey-d-luffy": 26,
+        "the-wings-zoro-sanji": 30,
+        "the-five-elders-st-marcus-mars": 14,
+        "seraphim-s-snake": 5,
+        "the-five-elders-st-ethanbaron-v-nusjuro": 17,
+        "ama-no-murakumo-sword-kizaru":2,
+        "flame-emperor-sabo": 4,
+        "animal-kingdom-pirates-lead-performer-king": 13,
+        "father-and-daughter-kuma-bonny": 11,
+        "blackbeard-pirates-kuzan": 9,
+        "unexpected-collaboration-rob-lucci": 3,
+        "winner-island-trafalgar-law": 4,
+        "great-pirate-edward-newgate": 3,
+        "great-pirate-gol-d-roger": 3,
+        "egghead-tony-tony-chopper": 1,
+        "the-four-emperors-marshall-d-teach": 9,
+        "future-where-i-m-the-most-free-jewelry-bonney": 29,
+        "legendary-hero-monkey-d-garp": 1,
+        "navy-hq-sword-prince-grus": 4,
+        "navy-hq-sword-koby": 3,
+        "navy-hq-lt-commander-helmeppo": 1,
+        "egghead-franky": 1,
+        "kaido-s-son-yamato": 1,
+        "impel-down-prisoner-bon-clay-aka-mr-2": 1,
+        "genius-scientist-vegapunk": 1,
+        "battle-of-monsters-on-onigashima-kaido": 1,
+        "firetank-pirates-godfather-capone-bege": 1,
+        "sick-sick-fruit-doc-q": 1
+    },
+} satisfies CharacterUsageSnapshot;
